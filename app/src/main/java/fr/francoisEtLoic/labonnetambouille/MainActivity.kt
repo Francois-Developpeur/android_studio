c60ccd604533c8a91cc4f9340b363fbc6e770ec9
Francois-Developpeur <francoisbierbaum@gmail.com>
2022-06-16 11:46:00 +0200

    Test240

--- app/src/main/java/fr/francoisEtLoic/labonnetambouille/MainActivity.kt
+++ app/src/main/java/fr/francoisEtLoic/labonnetambouille/MainActivity.kt
@@ -2,10 +2,20 @@ package fr.francoisEtLoic.labonnetambouille
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.francoisEtLoic.labonnetambouille.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // injecter le fragment dans notre boite (fragment container)
+        val transaction = supportFragmentManager.beginTransaction()
+        //stocke une valeur, supportFragmentManager permet de gérer les fragments sur android, de les manipuler
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+        //permet d'envoyer les changements
     }
 }
---
+++ app/src/main/java/fr/francoisEtLoic/labonnetambouille/adapter/RecipeAdapter.kt
@@ -0,0 +1,33 @@
+package fr.francoisEtLoic.labonnetambouille.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.francoisEtLoic.labonnetambouille.R
+
+class RecipeAdapter : RecyclerView.Adapter<RecipeAdapter.ViewHolder>() {
+
+    //boite pour ranger tout les composants à controler
+    class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        val recipeImage = view.findViewById<ImageView>(R.id.image_item)
+        //image de la recipe
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_horizontal_recipe, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 9
+    }
+}
